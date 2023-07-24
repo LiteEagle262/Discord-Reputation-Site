2bc5b1bd68a22f87f87584787677ee6356baa2f1
Ayden Fleming <48303729+LiteEagle262@users.noreply.github.com>
2023-07-24 11:42:08 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Site Settings
+$yourname = "Eagle";
+$apikey = "eagleishot";
+$faviconurl = "";
+
+// Theme colors
+$primaryColor = "#0073ff";
+$titleTextColor = "#fff";
+$tableColor = "#daab2d";
+$scrollbarColor = "#daab2d";
+$scrollbar_hover_color = "#bb9326";
+
+// Discord server link
+$discordServerLink = "your_discord_server_invite_link_here";
+?>
