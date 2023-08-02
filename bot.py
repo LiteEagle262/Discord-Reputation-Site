7d081e141a37b582e73155f5cda2bbb7d5644d2c
Ayden Fleming <48303729+LiteEagle262@users.noreply.github.com>
2023-08-02 18:31:15 -0400

    Update bot.py
    Updated to use message event instead of commands so it just gets all messages in the channel instead of using a prefix

--- bot.py
+++ bot.py
@@ -4,23 +4,21 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='+', help_command=None, intents=discord.Intents.all())
 
 ### -- EDIT THIS LINE -- ###
-vouch_channel_id = 1132899535371370597
-endpoint_url = ""
-api_key = "eagleishot"
-token = ""
-
-@bot.command()
-async def vouch(ctx, *args):
-    msg = " ".join(args)
-    if ctx.channel.id != vouch_channel_id:
-        await ctx.reply(f"You can't use this command here, only in <#{vouch_channel_id}>")
-        await ctx.message.delete()
-        return
-    if len(msg) > 150:
-        await ctx.author.send("Your message is too long, please keep it under 150 characters.")
-        await ctx.message.delete()
-        return
-    r = requests.post(endpoint_url, json = {"discord_tag": ctx.author.name,"discord_id": ctx.author.id,"vouch_content": {"message_id": str(ctx.message.id),"content": "vouch " + msg},"api_key": api_key})
-    await ctx.reply("Thank you!")
+token = "yourbottoken"
+yourid = 123
+vouch_channel_id = 123
+endpoint_url = "yoursiteurl.com/endpoint.php"
+api_key = "yourapikey"
+    
+@bot.event
+async def on_message(message):
+    if message.channel.id == vouch_channel_id and message.author.id != bot.user.id and message.author.id != yourid:
+        if len(message.content) > 250: # just cus looks better on site
+            await message.author.send("Your message is too long, please keep it under 250 characters.")
+            await message.delete()
+            return
+        r = requests.post(endpoint_url, json = {"discord_tag": message.author.name,"discord_id": message.author.id,"vouch_content": {"message_id": str(message.id),"content": message.content},"api_key": api_key})
+        await message.reply("Thank you!")
+    await bot.process_commands(message) # So that you can add regular commands as well
 
 bot.run(token)
